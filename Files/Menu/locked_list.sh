a3ef3d6ff84e5aeade0ae63de6db5bf14825490c
phcu-badbox <56748284+phcu-badbox@users.noreply.github.com>
2020-04-25 22:55:01 +0800

    Add files via upload

---
+++ Files/Menu/locked_list.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script by : BADBOX
+if [ -f "$FILE" ];
+then
+cd
+nano /etc/Locked_List.txt
+else
+cd
+nano /etc/Locked_List.txt
+fi
---
+++ Files/Menu/menu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Script by : BADBOX
+clear
+echo -e "\e[0m                                                   "
+echo -e "\e[93m           AutoScriptVPS by  BADBOX           "
+echo -e "\e[93m                  www.phcracker.net                "
+echo -e "\e[0m                                                   "
+echo -e "\e[93m        COMMANDS [menu/accounts/options/server]            "
+echo -e "\e[0m                                                   "
